2037cfed8a8d823e70ffce68f9a5ca00a5bf9c8b
Jaireua <jairenriquez30@gmail.com>
2025-03-08 17:33:07 -0500

    add

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirecT
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
