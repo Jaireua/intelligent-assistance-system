09f3ae94b298c82aca79521eb20262f86de42aba
Jaireua <jairenriquez30@gmail.com>
2025-03-08 19:42:54 -0500

    add

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 
 # Create your views here.
 def home(request):
@@ -16,10 +18,22 @@ def signup(request):
                 user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
                 user.save()
                 login(request, user)
-                return redirect('tasks')
+                return redirect('signin')
             except IntegrityError:
                 return render(request, 'signup.html', {'form': UserCreationForm, "error": "Username already exists."})
         return  HttpResponse("Passwords do not match.")
 
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm})
+    else:
+        user = authenticate (request, username=request.POST["username"], password=request.POST["password"])
+        if user is None:
+            return render(request, 'signin.html', {'form': AuthenticationForm, "error": "Invalid credentials."})
+        else:
+            login(request, user)
+            return redirect('tasks')
+
 def tasks(request):
     return render(request, 'tasks.html')
+
--- face_auth/urls.py
+++ face_auth/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
     path('tasks/', views.tasks, name='tasks'),
 ]
