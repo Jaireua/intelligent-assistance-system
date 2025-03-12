820412234cf9a8c991bca60504c5ad4b675c33c8
Jaireua <jairenriquez30@gmail.com>
2025-03-12 15:54:24 -0500

    Created view signup

---
+++ accounts/static/js/singup.js
@@ -0,0 +1,12 @@
+// Encapsulation code JS
+document.addEventListener('DOMContentLoaded', function () {
+    // Get references to HTML elements
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const captureButton = document.getElementById('capture');
+    const signupForm = document.getElementById('signup-form');
+    const message = document.getElementById('message');
+    let capturedImage = null;
+
+    // Access the camera and start the video stream
+});
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 # Import libraries
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 # View signup
+@csrf_exempt
 def signup(request):
     return render(request, 'signup.html') 
